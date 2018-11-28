ed57642436e1102eb215417114dd71336fe35b1c
Anthony Ngugi <anthonyjude554@gmail.com>
2018-11-28 04:18:07 +0300

    UPDATES
    This commit includes a responsive interface and an active service worker.
    
    I have added a missing ARIA label to the restaurant.html file.
    
    The previous commit had some white space around the app.
    This commit has been optimized to get rid of the whitespace.
    The minimum width for proper scaling for this web app is 295px.

--- js/registerServiceWorker.js
+++ js/registerServiceWorker.js
@@ -1,10 +1,10 @@
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register("/cacheManager.js")
+      .register("/sw.js")
       .then (reg => {
         console.log("Service worker registration succcessful:" + reg.scope);
       })
       .catch(error => {
         console.log("Registration failed: " + error);
       });
-  }
+  } 
